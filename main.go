c90bfc149d064ab362b2f61416bfec749430f66b
hugo <hugo_abonizio@hotmail.com>
2016-06-07 13:04:18 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "log"
+  "os/exec"
+  // "net/http"
+  "github.com/gorilla/websocket"
+  "github.com/kr/pty"
+)
+
+func main() {
+  c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/?type=terminal", nil)
+  if err != nil {
+    log.Fatal("dial:", err)
+  }
+
+  defer c.Close()
+
+  cmd := exec.Command("bash")
+  f, err := pty.Start(cmd)
+  if err != nil {
+      panic(err)
+  }
+
+  go func() {
+    // f.Write([]byte("ls\n"))
+    // f.Write([]byte{4}) // EOT
+    for {
+      _, command, err := c.ReadMessage()
+      if err != nil {
+        log.Println("read:", err)
+        return
+      }
+      fmt.Println("executing", string(command))
+      f.Write(command)
+    }
+  }()
+
+
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {
+    data := scanner.Bytes()
+    // fmt.Println(">>", data, "<<")
+    c.WriteMessage(websocket.TextMessage, data)
+  }
+}
